d9516630a0ccb91c97b6ad5f9e72c5316df32171
chenzelin <chenzelinmail@gmail.com>
2017-09-06 08:25:58 +0800

    Signed-off-by: chenzelin <chenzelinmail@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var net = require('net');
 var url = require('url');
-var _host = '6fc87c81.ngrok.io'
+var _host = '6ce95c60.ngrok.io'
 function request(cReq, cRes) {
     var u = url.parse(cReq.url);
     cReq.headers.host = _host;
