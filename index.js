4775afae3454da3cc3061608ca11b30d9c66c0b1
chenzelin01 <734880901@qq.com>
2018-04-12 20:08:59 +0800

    ngrok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var net = require('net');
 var url = require('url');
-var _host = '5352caf8.ngrok.io'
+var _host = 'a7717bd5.ngrok.io'
 function request(cReq, cRes) {
     var u = url.parse(cReq.url);
     cReq.headers.host = _host;
